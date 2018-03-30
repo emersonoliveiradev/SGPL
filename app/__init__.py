34be26c0864675570eb65e3706171bf248dec083
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-03-30 20:50:01 -0300

    Atualizado

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_restful import Api
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager
 
 #from marshmallow import Schema
 
+#Instância do arquivo de configuração
 app = Flask(__name__)
 app.config.from_object('config')
 
+#Instância do banco de dados
 db = SQLAlchemy(app)
-api = Api(app)
 migrate = Migrate(app, db)
 
+#Instância do banco do arquivo de gerenciamento
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server(host="0.0.0.0", port=9000))
 
+#Instância da API
+api = Api(app)
+
+#Instância da Login-Manager
+lm = LoginManager(app)
 
 #Não esquecer dessa linha, sem ela as tabelas não são criadas com o migrate
 from app import  views
--- app/controllers/default_routes.py
+++ app/controllers/default_routes.py
@@ -5,6 +5,7 @@ from app.models.forms import LoginForm, ProdutoForm, UsuarioForm, SetorForm, For
 #para o login
 from app.models.tables import Usuario
 
+
 #from app.models.tables import Setor
 #from app.models.tables import Usuario
 
@@ -17,45 +18,11 @@ def index():
     return render_template('index.html', produto_form = produto_form)
 
 
-@app.route("/logger", methods=['POST'])
-def logger1():
-    error = None
-    usuario = db.session.query(Usuario).filter_by(nome=request.form['nome']).first()
-    if request.method == 'POST':
-        if not usuario:
-            error = 'Credencial Invalida!'
-        else:
-            print(request.form['nome'])
-            return redirect(url_for('index'))
-
-    return render_template('index.html', error=error)
-
-'''
-def logger2():
-@app.route("/logger/nome", methods=['GET'])
-    usuario = db.session.query(Usuario).filter_by(nome=request.form['nome']).first()
-    if request.method == 'GET':
-        if not usuario:
-            error = 'Credencial Invalida!'
-        else:
-            print(request.form['nome'])
-            return redirect(url_for('index'))
-
-    return render_template('index.html', error=error)
-'''
-
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
-
-
-@app.route("/logout")
-def logout():
-    flash("Voce precisa estar logado!")
-    #session.pop['logged_in', None]
-    return redirect(url_for('login'))
+    form_login = LoginForm()
+    return render_template('login.html', form_login=form_login)
 
 
 
